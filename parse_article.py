2c36e371dc220252d4b75bda18c8a8196fc2dafb
jackyccheng <jackyccheng@users.noreply.github.com>
2020-04-19 16:31:19 +0100

    Updating code
    Rewrote how to remove non-characters, etc.

--- parse_article.py
+++ parse_article.py
@@ -1,4 +1,5 @@
-import bs4, requests
+from bs4 import BeautifulSoup
+import requests
 import pandas as pd
 import argparse
 
@@ -20,29 +21,29 @@ def get_article(link):
     link = requests.get(link)
     link.raise_for_status()
     link.encoding = 'utf-8'
-    parsed = bs4.BeautifulSoup(link.text, "html.parser")
+    parsed = BeautifulSoup(link.text, "html.parser")
 
     # Splits out lines by html paragraph, then into a list
     paragraph = ['p']
     txt = [t for t in parsed.find_all(text=True) if t.parent.name in paragraph]
-    characters = list("".join(txt))
+    everything = list("".join(txt))
+
+    # Removing some things from full list
+    removewords = ['。','，',' ',
+                   '.','"','、',
+                   '”',',','%']
+    resultwords = [t for t in everything if t not in removewords]
+    char_et_al = ' '.join(resultwords)
 
     # Removes digits from list
-    char_x_int = [x for x in characters if not (x.isdigit())]
+    characters = [x for x in char_et_al if not (x.isdigit())]
 
     # Turns list into dataframe for easier manipulation
-    df = pd.DataFrame(char_x_int)
+    df = pd.DataFrame(characters)
     df.rename(columns = {df.columns[0]:'char'}, inplace=True)
 
-    # Manipulates to exclude certain characters and return top 20 by frequency
-    final_df = df[(df.char != '。') & (df.char != '，') & (df.char != ' ')
-              & (df.char != '.') & (df.char != '"') & (df.char != '、')
-              & (df.char != '”') & (df.char != ',')]
-
-    top_df = final_df['char'].value_counts().head(20)
-
+    top_df = df['char'].value_counts().head(20)
     print(top_df)
 
 if __name__ == '__main__':
-
     get_article(link=options.link)
